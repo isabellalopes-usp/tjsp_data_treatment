b80502ab471e11503e4be928166159fb1e2df72d
Isabella Ferreira Lopes <isabella.lopes6@usp.br>
2023-05-15 13:46:00 -0300

    Update mark_new_results.py

--- mark_new_results.py
+++ mark_new_results.py
@@ -1,15 +1,17 @@
+#importa pacotes e bibliotecas
 import pandas as pd
 from google.colab import drive
 drive.mount('/content/drive')
 
-
+#cria lista de termos que obtiveram resultados, de acordo com a alocacao no diretorio
 planilhas=[]
 todos = pd.read_excel("/content/drive/termos_utilizados.xlsx")
 t_total = todos['termo']
 for t in t_total:
   if os.path.exists('/content/drive/com-acordaos-'+t+'.xlsx'):
     planilhas.append(t)
-    
+
+#salva novo dataframe no drive    
 def salva():
   #atualiza colunas
   tabela = p_new
@@ -17,10 +19,11 @@ def salva():
 
   final = pd.DataFrame(tabela)
   (final.sort_values(by='relator')).to_excel('/content/drive/com-acordaos-',index = False);
-  
+ 
+#compara processos da planilha antiga com os da planilha nova
  for a in planilhas:
   lista_new=[]
-  p_old = pd.read_excel('/content/drive/com-acordaos-'+a+'.xlsx')
+  p_old = pd.read_excel('/content/drive/old/com-acordaos-'+a+'.xlsx')
   p_new = pd.read_excel('/content/drive/com-acordaos-'+a+'.xlsx')
   todos = pd.read_excel("/content/drive/termos_utilizados.xlsx")
   p1 = p_old['processo']
@@ -40,7 +43,7 @@ def salva():
         tem = 1
         num = j
     if(tem==0):
-      lista_new.append('*'+i)
+      lista_new.append('*'+i) #marca com * os novos processos
     else:
       lista_new.append(num)
       tem = 0
